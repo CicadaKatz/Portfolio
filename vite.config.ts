83c2571816cb645a13049aeeae8a681fb3f28452
Leonid Mehandzhijski <leonidmehandzijski@gmail.com>
2025-05-06 15:42:14 +0200

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/', // This will be your GitHub Pages base URL
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 }); 
