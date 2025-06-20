7442963fffa6cde4d062647340df5a853d184123
Leonid Mehandzhijski <leonidmehandzijski@gmail.com>
2025-06-20 15:56:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
   optimizeDeps: {
-    include: [
-      'ldrs',
-    ],
+    include: ['ldrs/react'],
   },
   build: {
     outDir: 'dist',
@@ -15,12 +14,12 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined
-      }
-    }
+        manualChunks: undefined,
+      },
+    },
   },
   server: {
     port: 3000,
-    host: true
-  }
+    host: true,
+  },
 });
